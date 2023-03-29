abb5f15e3a76542d14cd57e83c4f848d5d61f9f3
CatMizu <zhrmghgjht@gmail.com>
2023-03-29 09:01:15 -0400

    install react-route

--- jh-clothing/src/App.js
+++ jh-clothing/src/App.js
@@ -1,12 +1,12 @@
 import Home from "./routers/home/home.component.jsx"
 import { Routes, Route } from 'react-router-dom'
-
+// testing
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes>
   );
-}
+} 
 
 export default App;
